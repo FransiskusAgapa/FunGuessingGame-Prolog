48a19481a534308d39aa87c01fb3edccc483bd7e
Fransiskus Agapa <agapaf@spu.edu>
2024-12-25 09:05:02 -0800

    first push baby

---
+++ game.pl
@@ -0,0 +1,11 @@
+welcome_champ :-
+    write("Hey Champion!"), nl.
+
+
+% how to run program
+% install swl-prolog file
+% add to path
+% --
+% open terminal > go to where the file is located
+% run the file > swipl game.pl (filename)
+% ?- name_of_function to execute
