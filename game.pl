b18e57d285f70dfe6d05d515fd0a6ac184a56ca2
Fransiskus Agapa <agapaf@spu.edu>
2024-12-27 05:43:42 -0800

    added function to ask user input

--- game.pl
+++ game.pl
@@ -1,11 +1,19 @@
+% Welcome the champion
 welcome_champ :-
     write("Hey Champion!"), nl.
 
+% Get guessing limit from user
+% Pass in a variable to catch the input value
+get_user_guess_limit(Limit) :- % 'Limit' catch input value and act as 'return' value
+    write("What is the guess limit you'd like it to be? "),
+    read(Limit), % read user input into variable 'Limit'
+    write("Your guess limit is: "), write(Limit), nl,
+    write("Your ")
 
-% how to run program
-% install swl-prolog file
-% add to path
-% --
-% open terminal > go to where the file is located
-% run the file > swipl game.pl (filename)
-% ?- name_of_function to execute
+
+% How to run program
+% 1. Install SWI-Prolog
+% 2. Add it to your system's PATH
+% 3. Open terminal and navigate to the directory where this file is located
+% 4. Run the file: swipl game.pl
+% 5. Call the function in the Prolog REPL: ?- welcome_champ. or ?- get_user_guess_limit.
