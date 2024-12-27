3516b35345bf76ab665691e731784b3ce7b10061
Fransiskus Agapa <agapaf@spu.edu>
2024-12-27 07:21:41 -0800

    simple function to start game added

--- game.pl
+++ game.pl
@@ -2,14 +2,15 @@
 welcome_champ :-
     write("Hey Champion!"), nl.
 
-% Get guessing limit from user
-% Pass in a variable to catch the input value
-get_user_guess_limit(Limit) :- % 'Limit' catch input value and act as 'return' value
-    write("What is the guess limit you'd like it to be? "),
-    read(Limit), % read user input into variable 'Limit'
-    write("Your guess limit is: "), write(Limit), nl,
-    write("Your ")
+% Generate a random number to guess | between two specified bounds, inclusive.
+generate_secret_number(Limit, Key) :-
+    random_between(1, Limit, Key).
 
+start :-
+    write("Input a guess limit: "),
+    read(Limit),nl,
+    write("Guess a number between 1 to "),
+    write(Limit).
 
 % How to run program
 % 1. Install SWI-Prolog
